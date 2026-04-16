93aed39fbf2046c5a6de3cbd06be69ca3e794a07
Andrea-Bertarione <andrea.bertarione@gmail.com>
2026-04-16 23:26:03 +0200

    Added the Sword_Sage NPC with the OpenForgeUIInteraction. Currently WIP as it directly opens the ForgeUI while i would like it to have a dialogue/Cutscene

--- src/main/java/dev/VoxelTales/Events/VoxelPlayerDisconnectEvent.java
+++ src/main/java/dev/VoxelTales/Events/VoxelPlayerDisconnectEvent.java
@@ -1,11 +1,12 @@
 package dev.VoxelTales.Events;
 
+import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import dev.VoxelTales.Registries.VoxelCacheRegistry;
 import dev.VoxelTales.VoxelTalesPlugin;
 
-public class PlayerDisconnectEvent {
-    public static void onPlayerDisconnect(com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent event) {
+public class VoxelPlayerDisconnectEvent {
+    public static void onPlayerDisconnect(PlayerDisconnectEvent event) {
         PlayerRef playerRef = event.getPlayerRef();
 
         VoxelTalesPlugin.get().removeLockedSlot(playerRef.getUuid());
